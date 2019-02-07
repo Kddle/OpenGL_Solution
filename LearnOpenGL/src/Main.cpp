d312739f48fde61f3b9dd76acd6879b80ad4cb0e
Victor <victor.schirmann@gmail.com>
2019-02-07 23:57:14 +0100

    Display Window

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -1,7 +1,72 @@
+#include <iostream>
 #include<glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main()
 {
+	// GLFW's OpenGL Initialisation
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Window creation
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make our window the main context on the current thread
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLAD before calling any OpenGL functions
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to intialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set the viewport (We have to tell OpenGL the size of the rendering window so OpenGL knows how we want to display the data and coordinates with respect to the window.)
+	glViewport(0, 0, 800, 600);
+
+	// Register our callback function called "on window resized"
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+
+		// rendering commands
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// swap the color buffer
+		glfwSwapBuffers(window);
+		// Checks if any events are triggered (mouse, keyboard ...)
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+// callback function on the window that gets called each time the window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
